8c5cdf149c685af900652cc43af7dae3d2886a19
Mel DeJesus <meldejesus22@gmail.com>
2019-02-15 10:13:07 -0500

    added tests

--- test/front/canary.test.js
+++ test/front/canary.test.js
@@ -2,19 +2,7 @@
 var assert = chai.assert;
 // var hello = require("../../backend/hello");
 
-describe("canary test", function() {
-  // A "canary" test is one we set up to always pass
-  // This can help us ensure our testing suite is set up correctly before writing real tests
-  // it("should pass this canary test", function() {
-  //   expect(true).to.be.true;
-  // });
-
-  // it("should return hello", function() {
-  //   var result = hello();
-  //   console.log("result is: ", result);
-  //   assert.deepEqual(result, "hello");
-  // });
-
+describe("test 'testing' function for low / hi return", function() {
   it("if parameter less than 5, it should return low", function() {
     var result = testing(3);
     console.log("result is: ", result);
